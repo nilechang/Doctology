d45c5b962fe3d2b2b3fb20310e44bcc298f9a463
Nile Chang <nile.chang@gmail.com>
2013-12-30 00:46:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doctology::Application.config.secret_key_base = '6b1ab3291283a4781892fec05e7677c2193f1ef21b2e0b22da0e18ec39b3d52c23e272aa97ac28b747e0c91bdd2c86ce0abc0c422c11356fc9e70c5c98b286ec'
